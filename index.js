bd353e3f9e063f92a363c96c4355ef4e249b0d51
leleo.viana <90351727+arielSenai@users.noreply.github.com>
2021-09-15 15:26:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ function calcular() {
   } else if (imc <= 29.9) {
     alert("Sobrepeso");
   } else if (imc <= 34.9) {
-    alert("Obesidade Grau I");
+    alert("Obesidade Grau 1");
   } else if (imc <= 39.9) {
-    alert("Obesidade Grau II");
+    alert("Obesidade Grau 2");
   } else if (imc >= 40) {
-    alert("Obesidade Grau III");
+    alert("Obesidade Grau 3");
   } else {
     alert("Digite um valor válido.");
   }
